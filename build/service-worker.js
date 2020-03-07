a5fe2f06aaada46f3b9ce31e75459208b274b447
puji <singolawu.corp@gmail.com>
2020-03-08 02:47:37 +0700

    update build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/adesty-akbar-wedding/precache-manifest.70af57305def41eec159385c31eac152.js"
+  "/adesty-akbar-wedding/precache-manifest.c7ae51b5082d2038cad65135d2311055.js"
 );
 
 workbox.clientsClaim();
