c7171a16f3a3ff26e11fba6e7fc32b44743107a8
puji <singolawu.corp@gmail.com>
2020-03-07 17:10:35 +0700

    Updates

---
+++ precache-manifest.b56f236177951d3a9af2f73000bc197f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ef43204e76c30709d8d",
+    "url": "/adesty-akbar-wedding/static/css/main.186e49c8.chunk.css"
+  },
+  {
+    "revision": "1ef43204e76c30709d8d",
+    "url": "/adesty-akbar-wedding/static/js/main.1ef43204.chunk.js"
+  },
+  {
+    "revision": "26e616b739329e3a57d6",
+    "url": "/adesty-akbar-wedding/static/js/1.26e616b7.chunk.js"
+  },
+  {
+    "revision": "43bcd2a0b133eaf8327d",
+    "url": "/adesty-akbar-wedding/static/js/runtime~main.43bcd2a0.js"
+  },
+  {
+    "revision": "760cfd21c8869c58f4fa5f5162c995bc",
+    "url": "/adesty-akbar-wedding/static/media/marble_gold_bg.760cfd21.jpg"
+  },
+  {
+    "revision": "de857ef46f8d1b30ed3a6fc34ff91a62",
+    "url": "/adesty-akbar-wedding/static/media/anno_ades.de857ef4.png"
+  },
+  {
+    "revision": "48b02996d08dc5e46d657cff639c9eca",
+    "url": "/adesty-akbar-wedding/static/media/pembuka1.48b02996.png"
+  },
+  {
+    "revision": "8f9d40e9bcea23a2aaecb5b13870fa4a",
+    "url": "/adesty-akbar-wedding/static/media/bride.8f9d40e9.jpg"
+  },
+  {
+    "revision": "c8b987d1dc7e83f4891a36e20df77876",
+    "url": "/adesty-akbar-wedding/static/media/groom.c8b987d1.jpg"
+  },
+  {
+    "revision": "fd58ca064ec94f319399b194c0b2a939",
+    "url": "/adesty-akbar-wedding/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/adesty-akbar-wedding/precache-manifest.b56f236177951d3a9af2f73000bc197f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/adesty-akbar-wedding/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
