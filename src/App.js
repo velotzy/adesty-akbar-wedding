fec128fb18e76361af719182dbe948e1427c2f3c
puji <singolawu.corp@gmail.com>
2020-03-15 05:00:20 +0700

    add music

--- src/App.js
+++ src/App.js
@@ -99,22 +99,7 @@ class App extends Component {
                 >
                     {children.map(child => child)}
                 </ReactSnapScroll>
-                {/* <div className="controls">
-                    <form>
-                        <h3>Transition</h3>
-                        <select onChange={ this.transitionChanged } name="transition">
-                            <option value="move-top-bottom">Move To Top / From Bottom</option>
-                            <option value="move-top-bottom-stagger">Move To Top Stagger / From Bottom</option>
-                            <option value="scale-down-top-bottom">Scale Down / From Bottom</option>
-                            <option value="scale-down-up">Scale Down / Scale Up</option>
-                            <option value="fold-top-bottom">Fold Top / From Bottom</option>
-                            <option value="cube-top-bottom">Cube Top / From Bottom</option>
-                        </select>
-                    </form>
-                </div> */}
-                {/* <div className="pagination">
-                    { index + 1 } / { children.length }
-                </div> */}
+                <audio src={require('./sound/bg_music.mp3')} autoPlay />
             </div>
         );
 
