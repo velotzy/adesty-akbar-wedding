8b9877309284dae8dd505d44863504bda7e535a3
puji <singolawu.corp@gmail.com>
2020-07-15 01:01:55 +0700

    Updates

---
+++ precache-manifest.321cbe21520f921c905ef9324d341532.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "8977b32eb61ce39ae3a9",
+    "url": "/adesty-akbar-wedding/static/css/main.fefadae6.chunk.css"
+  },
+  {
+    "revision": "8977b32eb61ce39ae3a9",
+    "url": "/adesty-akbar-wedding/static/js/main.8977b32e.chunk.js"
+  },
+  {
+    "revision": "5283edfdf4a3d176d9ad",
+    "url": "/adesty-akbar-wedding/static/css/1.8c571cd7.chunk.css"
+  },
+  {
+    "revision": "5283edfdf4a3d176d9ad",
+    "url": "/adesty-akbar-wedding/static/js/1.5283edfd.chunk.js"
+  },
+  {
+    "revision": "43bcd2a0b133eaf8327d",
+    "url": "/adesty-akbar-wedding/static/js/runtime~main.43bcd2a0.js"
+  },
+  {
+    "revision": "b02f0184340212825d42bdd1e6c384d8",
+    "url": "/adesty-akbar-wedding/static/media/photo_7.b02f0184.jpg"
+  },
+  {
+    "revision": "a7e63c100458bb5f7aa557db02258161",
+    "url": "/adesty-akbar-wedding/static/media/photo_2.a7e63c10.jpg"
+  },
+  {
+    "revision": "d25781f07b95888eea43601be8dd2836",
+    "url": "/adesty-akbar-wedding/static/media/photo_3.d25781f0.jpg"
+  },
+  {
+    "revision": "2eddc3863b41d173046af02dc60b50f4",
+    "url": "/adesty-akbar-wedding/static/media/photo_4.2eddc386.jpg"
+  },
+  {
+    "revision": "8b034b96684249034dd2b39f7b053319",
+    "url": "/adesty-akbar-wedding/static/media/photo_5.8b034b96.jpg"
+  },
+  {
+    "revision": "5b7cdd99986d283e35ba38b8d11b5ab8",
+    "url": "/adesty-akbar-wedding/static/media/photo_8.5b7cdd99.jpg"
+  },
+  {
+    "revision": "0e12d9534cd0fdfb5afc4312a610f6c9",
+    "url": "/adesty-akbar-wedding/static/media/pict_7.0e12d953.png"
+  },
+  {
+    "revision": "81cf825a1b9b749bd6dcc811bf07bd7a",
+    "url": "/adesty-akbar-wedding/static/media/pict_2.81cf825a.png"
+  },
+  {
+    "revision": "94fb73ea5d9c5cf21c7f4788ac2ee404",
+    "url": "/adesty-akbar-wedding/static/media/pict_3.94fb73ea.png"
+  },
+  {
+    "revision": "f26bbc010766372fa67844484bc889c1",
+    "url": "/adesty-akbar-wedding/static/media/pict_4.f26bbc01.png"
+  },
+  {
+    "revision": "3f42388a29ad83b94000051442039b71",
+    "url": "/adesty-akbar-wedding/static/media/pict_5.3f42388a.png"
+  },
+  {
+    "revision": "27b1368ef73d56eb38ee538ab8ef3ff1",
+    "url": "/adesty-akbar-wedding/static/media/pict_8.27b1368e.png"
+  },
+  {
+    "revision": "96fc937ff1c2f3e99960de84bb471fc4",
+    "url": "/adesty-akbar-wedding/static/media/bg_gray.96fc937f.png"
+  },
+  {
+    "revision": "b186533df971e3829dfee6c2c4f8274a",
+    "url": "/adesty-akbar-wedding/static/media/cover.b186533d.png"
+  },
+  {
+    "revision": "48b02996d08dc5e46d657cff639c9eca",
+    "url": "/adesty-akbar-wedding/static/media/pembuka1.48b02996.png"
+  },
+  {
+    "revision": "e2417d7bccf2366bea736050a0f26444",
+    "url": "/adesty-akbar-wedding/static/media/bride.e2417d7b.png"
+  },
+  {
+    "revision": "7bdde95a3823ae86f624d47b457fbf44",
+    "url": "/adesty-akbar-wedding/static/media/groom.7bdde95a.png"
+  },
+  {
+    "revision": "c3a9455eb980d33c2a8d5fab6aa0efe6",
+    "url": "/adesty-akbar-wedding/static/media/top-left.c3a9455e.png"
+  },
+  {
+    "revision": "86e8ca66c1a8268c8fb37e9e5e074e9e",
+    "url": "/adesty-akbar-wedding/static/media/bottom-right.86e8ca66.png"
+  },
+  {
+    "revision": "376e25f7b1f7881bb0b4fbec3ebf4bdb",
+    "url": "/adesty-akbar-wedding/static/media/swipeup.376e25f7.gif"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/adesty-akbar-wedding/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/adesty-akbar-wedding/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/adesty-akbar-wedding/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/adesty-akbar-wedding/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "82fa46c12670a4dbefed251c1d5ae16b",
+    "url": "/adesty-akbar-wedding/index.html"
+  }
+];
