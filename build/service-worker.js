e48650ab18c9836528ebd67eccd57091b27a8943
puji <singolawu.corp@gmail.com>
2020-07-15 09:48:03 +0700

    update audio player

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/adesty-akbar-wedding/precache-manifest.add4b4b8a3c450f686f52525e294ec8c.js"
+  "/adesty-akbar-wedding/precache-manifest.321cbe21520f921c905ef9324d341532.js"
 );
 
 workbox.clientsClaim();
